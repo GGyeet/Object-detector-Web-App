f4f5ee612f6d5c70a49d92e1c6dd7400b86132c0
GG <105051795+GGyeet@users.noreply.github.com>
2023-06-06 19:54:47 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status="";
+objects=[];
 function preload(){
     img=loadImage('images.fridge.jpg');
 }
@@ -20,3 +21,48 @@ console.error();
 console.log(results);
 objects=results;
 }
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status!= ""){
+       for(i=0; i<objects.length; i++)
+       {
+        document.getElementById("status").innerHTML="status: object detected"
+        fill("#FF0000");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       }
+    }
+    fill("#FF0000");
+    text("Fridge", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+    
+    fill("#FF0000");
+    text("kettle", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+        
+    fill("#FF0000");
+    text("roomba", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+        
+    fill("#FF0000");
+    text("toaster", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+        
+    fill("#FF0000");
+    text("washing Machine", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+    }
